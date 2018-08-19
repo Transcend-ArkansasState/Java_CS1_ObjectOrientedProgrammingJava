80e7801ba7e93ee1eeea55f29597979663246b92
adamclmns <adam@adamclmns.com>
2018-08-19 10:28:32 -0500

    object serialization demo documented more.

--- demo-object-serialization/src/main/java/edu/transcend/objects/SerializeObjects.java
+++ demo-object-serialization/src/main/java/edu/transcend/objects/SerializeObjects.java
@@ -10,9 +10,11 @@ import java.util.Scanner;
 public class SerializeObjects {
 
     public static void main(String args[]) {
+        // Create our gson object that parses JSON and Java Objects (POJOs)
         Gson gson = new Gson();
-
+        // If no ARgs... this is just a default method, so we can kinda test everything out
         if (args.length <= 0) {
+            // We do a lot here in sequence. This is just a procedural run through of what the program 'can' do
             String jsonAuthor = gson.toJson(createAuthor());
             System.out.println("Serialized Author as JSON");
             System.out.println(jsonAuthor);
@@ -25,24 +27,31 @@ public class SerializeObjects {
             saveBookToJSONFile("test.json", bookToSave);
             System.out.println("Saved JSON Book");
             System.out.println("Loading JSON Book");
-
             SampleBook newBook = loadBookFromJSONFile("test.json");
             System.out.println("Loaded JSON Book is: " + gson.toJson(newBook));
-        } else if (args[0].equalsIgnoreCase("createSaveBook")) {
+            // [Java Invocation Command] String[] args
+            // java -jar JarName.jar args[0] args[1] args[2]...
+            // java SerializeObjects.class args[0] args[1] args[2]...
+        } else if (args[0].equalsIgnoreCase("createSaveBook")) { // java -jar object-serialization.jar createSaveBook myBook.json
             SampleBook book = createBookFromConsole();
             saveBookToJSONFile(args[1], book);
-        } else if (args[0].equalsIgnoreCase("loadPrintBook")) {
+        } else if (args[0].equalsIgnoreCase("loadPrintBook")) { // java -jar object-serialization.jar loadPrintBook myBook.json
             SampleBook book = loadBookFromJSONFile(args[1]);
             System.out.println(book);
-        } else if (args[0].equalsIgnoreCase("createSaveAuthor")) {
+        } else if (args[0].equalsIgnoreCase("createSaveAuthor")) { // java -jar object-serialization.jar createSaveAuthor myAuthor.json
             SampleAuthor author = createAuthorFromConsole();
             saveAuthorToJSONFile(args[1], author);
-        } else if (args[0].equalsIgnoreCase("loadPrintAuthor")) {
+        } else if (args[0].equalsIgnoreCase("loadPrintAuthor")) { //// java -jar object-serialization.jar loadPrintAuthor myAuthor.json
             SampleAuthor author = loadAuthorFromJSONFile(args[1]);
             System.out.println(author);
         }
     }
 
+    /**
+     * Creates a Random Author Object with a random selection of first and last names.
+     *
+     * @return
+     */
     private static SampleAuthor createAuthor() {
         String[] firstNames = new String[]{"Mark", "Jane", "Ray", "Mary"};
         String[] lastNames = new String[]{"Twain", "Austen", "Bradburry", "Shelly"};
@@ -54,6 +63,10 @@ public class SerializeObjects {
         return author;
     }
 
+    /**
+     * Creates a random Book Object from a list of titles, and contains a random author object also.
+     * @return
+     */
     private static SampleBook createBook() {
         String[] titles = new String[]{"Tom Sawyer", "Pride and Prejudice", "451", "Frankenstein"};
 
@@ -67,16 +80,29 @@ public class SerializeObjects {
 
     }
 
+    /**
+     *
+     * @return
+     */
     private static SampleBook createBookFromConsole() {
         return createBook();
         // TODO: Prompt for values to fill out a book.
     }
 
+    /**
+     *
+     * @return
+     */
     private static SampleAuthor createAuthorFromConsole() {
         // TODO: Prompt for values to fill out an Author
         return createAuthor();
     }
 
+    /**
+     * Loads a SampleBook Object from a JSON File of given filename
+     * @param filename name of the .json file
+     * @return The Book Object serialized in the file
+     */
     private static SampleBook loadBookFromJSONFile(String filename) {
         Gson gson = new Gson();
         File file = new File(filename);
@@ -95,6 +121,11 @@ public class SerializeObjects {
         return book;
     }
 
+    /**
+     * Loads a SampleAUthor object from a JSON file of given filename
+     * @param filename name of the .json file
+     * @return The Object serialized in the file
+     */
     private static SampleAuthor loadAuthorFromJSONFile(String filename) {
         Gson gson = new Gson();
         File file = new File(filename);
@@ -113,19 +144,34 @@ public class SerializeObjects {
         return author;
     }
 
+    /**
+     * Converts the provided Object to a JSON String value and then saves to file
+     * @param filename
+     * @param book
+     */
     private static void saveBookToJSONFile(String filename, SampleBook book) {
         Gson gson = new Gson();
         String bookAsString = gson.toJson(book);
         saveStringToFile(filename, bookAsString);
     }
 
-
+    /**
+     * Converts the provided Object to a JSON String value and then saves to file
+     * @param filename
+     * @param author
+     */
     private static void saveAuthorToJSONFile(String filename, SampleAuthor author) {
         Gson gson = new Gson();
         String authorAsString = gson.toJson(author);
         saveStringToFile(filename, authorAsString);
     }
 
+    /**
+     * This method writes a string to a file. This is just to keep the other methods from repeating the same
+     * FileWriter code and try/catch.
+     * @param filename
+     * @param stringToWrite
+     */
     private static void saveStringToFile(String filename, String stringToWrite) {
 
         try (FileWriter fw = new FileWriter(filename)) {
--- web-scraping/src/main/java/edu/asu/transcend/http/HttpScraper.java
+++ web-scraping/src/main/java/edu/asu/transcend/http/HttpScraper.java
@@ -1,22 +1,100 @@
 package edu.asu.transcend.http;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//http://www.vogella.com/tutorials/Logging/article.html
 public class HttpScraper {
+    /**
+     * Watch your Styles...
+     * <p>
+     * Variables marked as "static final" are constants.
+     * Static means No instance of the class owns the value, it's a static property of that type.
+     * Final means the value cannot be changed.
+     * It is a Java Convention that Constants are named in all caps, with underscores.
+     * <p>
+     * This convention is not always applied to Logger objects, but is always applied to
+     * String constants.
+     */
+    private static final Logger LOGGER = Logger.getLogger(HttpScraper.class.getName());
     private static final String DEFUALT_URL = "http://www.astate.edu/a/chancellor/index.dot";
 
     public static void main(String[] args) {
+        // ALL THE LOGS!
+        LOGGER.setLevel(Level.FINEST);
+
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
         HttpGet httpGet = new HttpGet(DEFUALT_URL);
         try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-            System.out.println("httpGet:  " + response.toString());
+            LOGGER.log(Level.FINE, "httpGet:  " + response.toString());
+            HttpEntity entity = response.getEntity();
+            // We get an input Stream from the Response Entity.
+            // We can use a Scanner to read that!
+            InputStream is = entity.getContent();
+            Scanner httpScanner = new Scanner(is);
+            StringBuilder httpBodyBuilder = new StringBuilder();
+
+            /**
+             *
+             * We use StringBuilder for large strings, or
+             * strings that we're going to add to and slice up a lot.
+             *
+             * StringBuilder has features that make working with
+             * large strings faster, and sometimes even easier to read in code
+             *
+             * The code will still work with String += String, but it won't be as
+             * efficient. The trick is knowing that the StringBuilder is a thing that exists,
+             * and knowing what it's used for.
+             *
+             * Programming is like learning the magic words and the ingredients to software.
+             *
+             *
+             */
+            while (httpScanner.hasNextLine()) {
+                httpBodyBuilder.append(httpScanner.nextLine());
+            }
+            // At this point, we have the full WebPage HTML. Let's scan it for patterns.
+            LOGGER.log(Level.FINE, httpBodyBuilder.toString());
+
+            /**
+             * Regular Expressions are a cross language way of describing patterns.
+             *
+             * RegEx, as it's commonly called, is used to detect patterns in
+             * text, primarily for validating input on forms, and enforcing formats
+             * on data entry. Regex can also be used to recognize data of certain types.
+             *
+             * Java uses it's own RegEx in the Pattern object.
+             *
+             * Here's a tutorial showing the code for validating North American phone numbers.
+             * https://howtodoinjava.com/regex/java-regex-validate-and-format-north-american-phone-numbers/
+             */
+            /**
+             * Oh, look! Stack Overflow has done this too
+             *
+             * - https://stackoverflow.com/a/18036919
+             *
+             */
+            String phoneRegEx = "\\(\\d{3}\\)\\s\\d{3}-\\d{4}"; // Thank you Stack Overflow!
+            Pattern pattern = Pattern.compile(phoneRegEx);
+            Matcher m = pattern.matcher(httpBodyBuilder.toString());
+            while (m.find()) {
+                LOGGER.log(Level.INFO, m.group());
+                // Note: There is only one phone number on this page.
+            }
 
         } catch (Exception ex) {
-            ex.printStackTrace();
+            LOGGER.log(Level.SEVERE, ex.getMessage());
         }
 
 
